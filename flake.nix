98ecd0cb2ed206dd1f9c4716de63657abda749d8
tete <tete@nixos.com>
2026-08-24 19:52:45 +0100

    First commit

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    inputs.import-tree.url = "github:denful/import-tree";
+
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; }
+  (inputs.import-tree ./modules);
+}
